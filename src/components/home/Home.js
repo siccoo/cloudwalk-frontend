8481c392a6995f51a1bc936b013cae4e2b2a7191
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-10-01 06:41:15 +0100

    structuring the jsx element in Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,39 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="starwar__container">
       <header>
-          <div>
-              <Link to="/">Star Wars Characters</Link>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-          </div>
+        <div>
+          <Link to="/">Star Wars Characters</Link>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </header>
+      <main>
+        <hr />
+        <div className="starwars__box__align">
+          <div>
+            <form>
+              <label>Filter By:</label>
+              <select>
+                <option value="">Planets</option>
+                <option value="">Planets</option>
+                <option value="">Planets</option>
+                <option value="">Planets</option>
+                <option value="">Planets</option>
+              </select>
+            </form>
+          </div>
+          <div>
+            <button type="button" disabled>Clear all</button>
+          </div>
+        </div>
+        <hr />
+      </main>
     </div>
   );
 };
