6d1210aef0e2ca3cc7d8a4b8cfd673f555cf58a1
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-10-02 06:53:36 +0100

    updates on Home.js & Home.scss & setting all states

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import swapiApi from "../../utils/swapi";
 
 import "./Home.scss";
 
 const Home = () => {
+  const [mapPlanets, setMapPlanets] = useState();
+  const [movies, setMovies] = useState();
+  const [state, setState] = useState(false);
+  const [characters, setCharacters] = useState([]);
+
   return (
     <div className="starwar__container">
       <header>
@@ -36,9 +42,7 @@ const Home = () => {
             </form>
           </div>
           <div>
-            <button type="button">
-              Clear all
-            </button>
+            <button type="button">Clear all</button>
           </div>
         </div>
         <hr />
@@ -46,16 +50,124 @@ const Home = () => {
           <p>All Characters</p>
           <div className="starwar__box__align__center">
             <div className="starwar__card">
-                <a href="/ja">
-                  <img src="" alt="" />
-                </a>
-                <div className="card__body">
-                  <h4>Name 2</h4>
-                  <h6>Homeworld/Planet Name</h6>
-                  <p>HEIGHT • 100M</p>
-                  <p>MASS • 100kg</p>
-                  <p>GENDER • FEMALE</p>
-                </div>
+              <a href="/ja">
+                <img src="" alt="" />
+              </a>
+              <div className="card__body">
+                <h4>Name 2</h4>
+                <h6>Homeworld/Planet Name</h6>
+                <p>HEIGHT • 100M</p>
+                <p>MASS • 100kg</p>
+                <p>GENDER • FEMALE</p>
+              </div>
+            </div>
+            <div className="starwar__card">
+              <a href="/ja">
+                <img src="" alt="" />
+              </a>
+              <div className="card__body">
+                <h4>Name 2</h4>
+                <h6>Homeworld/Planet Name</h6>
+                <p>HEIGHT • 100M</p>
+                <p>MASS • 100kg</p>
+                <p>GENDER • FEMALE</p>
+              </div>
+            </div>
+            <div className="starwar__card">
+              <a href="/ja">
+                <img src="" alt="" />
+              </a>
+              <div className="card__body">
+                <h4>Name 2</h4>
+                <h6>Homeworld/Planet Name</h6>
+                <p>HEIGHT • 100M</p>
+                <p>MASS • 100kg</p>
+                <p>GENDER • FEMALE</p>
+              </div>
+            </div>
+            <div className="starwar__card">
+              <a href="/ja">
+                <img src="" alt="" />
+              </a>
+              <div className="card__body">
+                <h4>Name 2</h4>
+                <h6>Homeworld/Planet Name</h6>
+                <p>HEIGHT • 100M</p>
+                <p>MASS • 100kg</p>
+                <p>GENDER • FEMALE</p>
+              </div>
+            </div>
+            <div className="starwar__card">
+              <a href="/ja">
+                <img src="" alt="" />
+              </a>
+              <div className="card__body">
+                <h4>Name 2</h4>
+                <h6>Homeworld/Planet Name</h6>
+                <p>HEIGHT • 100M</p>
+                <p>MASS • 100kg</p>
+                <p>GENDER • FEMALE</p>
+              </div>
+            </div>
+            <div className="starwar__card">
+              <a href="/ja">
+                <img src="" alt="" />
+              </a>
+              <div className="card__body">
+                <h4>Name 2</h4>
+                <h6>Homeworld/Planet Name</h6>
+                <p>HEIGHT • 100M</p>
+                <p>MASS • 100kg</p>
+                <p>GENDER • FEMALE</p>
+              </div>
+            </div>
+            <div className="starwar__card">
+              <a href="/ja">
+                <img src="" alt="" />
+              </a>
+              <div className="card__body">
+                <h4>Name 2</h4>
+                <h6>Homeworld/Planet Name</h6>
+                <p>HEIGHT • 100M</p>
+                <p>MASS • 100kg</p>
+                <p>GENDER • FEMALE</p>
+              </div>
+            </div>
+            <div className="starwar__card">
+              <a href="/ja">
+                <img src="" alt="" />
+              </a>
+              <div className="card__body">
+                <h4>Name 2</h4>
+                <h6>Homeworld/Planet Name</h6>
+                <p>HEIGHT • 100M</p>
+                <p>MASS • 100kg</p>
+                <p>GENDER • FEMALE</p>
+              </div>
+            </div>
+            <div className="starwar__card">
+              <a href="/ja">
+                <img src="" alt="" />
+              </a>
+              <div className="card__body">
+                <h4>Name 2</h4>
+                <h6>Homeworld/Planet Name</h6>
+                <p>HEIGHT • 100M</p>
+                <p>MASS • 100kg</p>
+                <p>GENDER • FEMALE</p>
+              </div>
+            </div>
+            <div className="starwar__card">
+              <a href="/ja">
+                <img src="" alt="" />
+              </a>
+              <div className="card__body">
+                <h4>Name 2</h4>
+                <h6>Homeworld/Planet Name</h6>
+                <p>HEIGHT • 100M</p>
+                <p>MASS • 100kg</p>
+                <p>GENDER • FEMALE</p>
+              </div>
             </div>
           </div>
         </section>
